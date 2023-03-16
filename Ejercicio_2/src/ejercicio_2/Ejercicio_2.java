b4b43dfeb178f62c06cfa4d11c1aa57c168caff4
Daniel Roa <droa@apgslt.com>
2023-03-15 19:48:48 -0500

    Ejercicio 2 - Finalizado·

--- Ejercicio_2/src/ejercicio_2/Ejercicio_2.java
+++ Ejercicio_2/src/ejercicio_2/Ejercicio_2.java
@@ -4,6 +4,11 @@
  */
 package ejercicio_2;
 
+import Entidades.Lavadora;
+import Entidades.Televisor;
+import Servicios.serviciosLavadora;
+import Servicios.serviciosTelevisor;
+
 /**
  *
  * @author droa
@@ -16,6 +21,20 @@ public class Ejercicio_2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        serviciosLavadora sL = new serviciosLavadora();
+        serviciosTelevisor sT = new serviciosTelevisor();
+        
+        System.out.println("LAVADORA");
+        Lavadora l1 = sL.crearLavadora();
+        System.out.println("TELEVISOR");
+        Televisor t1 = sT.crearTelevisor();
+        
+        l1.precioFinal();
+        t1.precioFinal();
+        
+        System.out.println("..:: PRECIOS FINALES ::..");
+        System.out.println("Precio Final Lavadora: "+String.format("%.2f",l1.getPrecio())+" USD");
+        System.out.println("Precio Final Televisor: "+String.format("%.2f",t1.getPrecio())+" USD");
         
         
     }
